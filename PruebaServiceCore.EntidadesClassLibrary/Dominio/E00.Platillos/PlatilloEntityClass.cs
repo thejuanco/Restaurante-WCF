3b7b01a238d15897ecf0c64f5c79e951a503918d
Juanco <juanffo874@gmail.com>
2025-08-14 23:19:04 -0600

    Agrega los atributos a la entidad del platillo

--- PruebaServiceCore.EntidadesClassLibrary/Dominio/E00.Platillos/PlatilloEntityClass.cs
+++ PruebaServiceCore.EntidadesClassLibrary/Dominio/E00.Platillos/PlatilloEntityClass.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PruebaServiceCore.EntidadesClassLibrary.Dominio.E00.Platillos
 {
-    internal class PlatilloEntityClass
+    [Serializable]
+    public class PlatilloEntityClass
     {
+        [DataMember]
+        public string platillo_Id { get; set; }
+
+        [DataMember]
+        public string platillo_Nombre { get; set; }
+        [DataMember]
+        public string platillo_Desc { get; set; }
+        [DataMember]
+        public decimal platillo_Precio { get; set; }
     }
 }
