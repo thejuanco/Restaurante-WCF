567d71793623aece9d18400cc8176a7f09cb4e64
Juanco <juanffo874@gmail.com>
2025-08-14 23:07:12 -0600

    Agrega las carpetas para platillos y crea su clase en conjunto con la entity class

---
+++ PruebaServiceCore.EntidadesClassLibrary/Dominio/E00.Platillos/Platillos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaServiceCore.EntidadesClassLibrary.Dominio.E00.Platillos
+{
+    internal class Platillos
+    {
+    }
+}
